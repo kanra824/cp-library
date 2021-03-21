105b5558528f9b9f849af1c5d7ca1ea3d5c8f2e3
kanra824 <mdyh2a380824@gmail.com>
2021-03-21 09:19:17 +0900

    test

--- test/data_structure/segment_tree.test.cpp
+++ test/data_structure/segment_tree.test.cpp
@@ -1,4 +1,25 @@
-//
-// Created by Md on 2021/03/21.
-//
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_B&lang=jp"
+
+#include "../../lib/template.cpp"
+#include "../../lib/data_structure/segment_tree.cpp"
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  cout << fixed << setprecision(10);
+
+  int n, q; cin >> n >> q;
+  SegmentTree st(n);
+  REP(i, q) {
+    int t; cin >> t;
+    int x, y; cin >> x >> y;
+    x--;
+    if(t == 0) {
+      st.update(x, y);
+    } else {
+      cout << st.query(x, y) << endl;
+    }
+  }
+
+}
 
