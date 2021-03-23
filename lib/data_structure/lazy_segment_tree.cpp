1548876b37eb780366290237d6ed554ff3b0b67c
kanra824 <mdyh2a380824@gmail.com>
2021-03-24 00:47:07 +0900

    Abstraction

--- lib/data_structure/lazy_segment_tree.cpp
+++ lib/data_structure/lazy_segment_tree.cpp
@@ -1,37 +1,68 @@
 // とりあえずrange update range sum
+template <class M, class OM>
 struct LazySegmentTree {
   int n;
-  vector<ll> node, lazy;
+  vector<M> node;
+  vector<OM> lazy;
 
-  LazySegmentTree(int sz) {
+  using F = function<M(M, M)>;
+  using G = function<M(M, OM)>;
+  using H = function<OM(OM, OM)>;
+
+  F f;
+  G g;
+  H h;
+
+  M e;
+  OM oe;
+
+  LazySegmentTree(int sz, F ff, G gg, H hh, M ee, OM oeoe):
+  f(ff),
+  g(gg),
+  h(hh),
+  e(ee),
+  oe(oeoe)
+  {
     n = 1;
     while(n < sz) {
       n <<= 1;
     }
-    node.resize(2*n, 0);
-    lazy.resize(2*n, INF);
+    node.resize(2*n, e);
+    lazy.resize(2*n, oe);
   }
 
-  void propagate(int a, int b, int k) {
-    if(lazy[k] != INF) {
-      node[k] += lazy[k];
-      if(2*k < 2*n) {
-        if(lazy[2*k] == INF) lazy[2*k] = lazy[k] / 2;
-        else lazy[2*k] += lazy[k] / 2;
-      }
-      if(2*k < 2*n) {
-        if(lazy[2*k+1] == INF) lazy[2*k+1] = lazy[k] / 2;
-        else lazy[2*k+1] += lazy[k] / 2;
-      }
-      lazy[k] = INF;
+  LazySegmentTree(int sz, F ff, G gg, H hh, M ee, OM oeoe, vector<M> v):
+  f(ff),
+  g(gg),
+  h(hh),
+  e(ee),
+  oe(oeoe)
+  {
+    n = 1;
+    while(n < sz) {
+      n <<= 1;
+    }
+    node.resize(2*n, e);
+    lazy.resize(2*n, oe);
+    REP(i, SZ(v)) {
+      node[i+n] = v[i];
+    }
+    RREP(i, n-1) {
+      node[i] = f(node[2*i], node[2*i+1]);
     }
   }
 
-  void update(int l, int r, int a, int b, int k, int x) {
+  void propagate(int a, int b, int k) {
+    node[k] = g(node[k], lazy[k]);
+    if(2*k < 2*n) lazy[2*k] = h(lazy[2*k], lazy[k]);
+    if(2*k+1 < 2*n) lazy[2*k+1] = h(lazy[2*k+1], lazy[k]);
+    lazy[k] = oe;
+  }
+
+  void update(int l, int r, int a, int b, int k, OM x) {
     propagate(a, b, k);
     if(l <= a && b <= r) {
-      if(lazy[k] == INF) lazy[k] = (b - a) * x;
-      else lazy[k] += (b - a) * x;
+      lazy[k] = h(lazy[k], x);
       propagate(a, b, k);
       return;
     }
@@ -41,29 +72,29 @@ struct LazySegmentTree {
 
     update(l, r, a, (a+b)/2, 2*k, x);
     update(l, r, (a+b)/2, b, 2*k+1, x);
-    node[k] = node[2*k] + node[2*k+1];
+    node[k] = f(node[2*k], node[2*k+1]);
   }
 
-  void update(int l, int r, int x) {
+  void update(int l, int r, OM x) {
     update(l, r, 0, n, 1, x);
   }
 
-  ll query(int l, int r, int a, int b, int k) {
+  M query(int l, int r, int a, int b, int k) {
     propagate(a, b, k);
 
     if(l <= a && b <= r) {
       return node[k];
     }
     if(b <= l || r <= a) {
-      return 0;
+      return e;
     }
 
-    ll val1 = query(l, r, a, (a+b)/2, 2*k);
-    ll val2 = query(l, r, (a+b)/2, b, 2*k+1);
-    return val1 + val2;
+    M val1 = query(l, r, a, (a+b)/2, 2*k);
+    M val2 = query(l, r, (a+b)/2, b, 2*k+1);
+    return f(val1, val2);
   }
 
-  ll query(int l, int r) {
+  M query(int l, int r) {
     return query(l, r, 0, n, 1);
   }
 
--- test/data_structure/lazy_segment_tree/range_add_range_sum.test.cpp
+++ test/data_structure/lazy_segment_tree/range_add_range_sum.test.cpp
@@ -8,7 +8,17 @@ int main() {
   ios::sync_with_stdio(false);
 
   int n, q; cin >> n >> q;
-  LazySegmentTree st(n);
+  using P = pair<ll, int>;
+  vector<P> v(n, {0, 1});
+  LazySegmentTree<P, ll> st(
+      n,
+      [](P a, P b){return make_pair(a.first + b.first, a.second + b.second);},
+      [](P a, ll b){return make_pair(a.first + a.second * b, a.second);},
+      [](ll a, ll b){return a + b;},
+      make_pair(0ll, 0),
+      0,
+      v
+      );
   REP(i, q) {
     int k; cin >> k;
     if(k == 0) {
@@ -18,7 +28,7 @@ int main() {
     } else {
       int a, b; cin >> a >> b;
       a--;
-      cout << st.query(a, b) << endl;
+      cout << st.query(a, b).first << endl;
     }
   }
 }
