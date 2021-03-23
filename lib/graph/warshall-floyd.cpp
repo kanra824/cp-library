64a5cb7d6cddce270eddfee175b036a6c37f465e
kanra824 <mdyh2a380824@gmail.com>
2021-03-24 01:52:57 +0900

    warshall floyd

---
+++ lib/graph/warshall-floyd.cpp
@@ -0,0 +1,14 @@
+template<class T>
+void warshall_floyd(vector<vector<T>> &d) {
+  int sz = SZ(d);
+  int now_inf = numeric_limits::max<T>();
+  REP(k, sz) {
+    REP(i, sz) {
+      REP(j, sz) {
+        if(d[i][k] != now_inf && d[k][j] != now_inf) {
+          chmin(d[i][j], d[i][k] + d[k][j]);
+        }
+      }
+    }
+  }
+}
